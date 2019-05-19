f241987e236c8803e531892007559d22997822c6
seeletraum <50781117+seeletraum@users.noreply.github.com>
2019-05-19 11:36:49 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCVkRcSiF0KmLCwmoo4edtw0vnSykCvFcc');
+define('APIKEY', 'AIzaSyD1pnHiGvvAEiHjSxqf_TSmA7FOoyoKhOs');
 define('GJ_CODE', 'US');
 define('SITE_NAME', 'y2b');
 define('TITLENAME', 'y2b');
-define('EN2DEKEY', '32123wwerwer323tgfdgds');
-define('EMAIL', '1334347212@qq.com');
-define('NAME', 'admin');
-define('PASSWORD', 'admin');
+define('EN2DEKEY', '32123wwedddadrwer323tgfdgds');
+define('EMAIL', '706642327@qq.com');
+define('NAME', 'seeletraum');
+define('PASSWORD', 'adm9304');
 define('LOGINHTML', '<!DOCTYPE html>
 <html lang="en">
 <head>
